06d049e9ff29dc2300702972d33b7db65a2e41d5
yuzh <lnc@mail.ustc.edu.cn>
2019-04-23 19:04:26 +0800

    change mxnet base dir

--- exp/__set_os_env__.py
+++ exp/__set_os_env__.py
@@ -1,7 +1,7 @@
 import os
 
-MXNET_DATA_COCO = os.path.join('/', 'userhome', '.mxnet','datasets', 'coco')
+MXNET_DATA_COCO = os.path.abspath(os.path.expanduser('~/.mxnet/datasets/coco'))
 
-os.environ['MXNET_HOME'] = os.path.join('/', 'userhome', '.mxnet')
+os.environ['MXNET_HOME'] = os.path.abspath(os.path.expanduser('~/.mxnet'))
 os.environ['MXNET_GLUON_REPO'] = 'https://apache-mxnet.s3.cn-north-1.amazonaws.com.cn/'
 os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
